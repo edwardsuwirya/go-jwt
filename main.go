799da13f379515053ef9a5676f17f9a2fecd30b2
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-11-29 16:25:42 +0700

    Manual Header

--- main.go
+++ main.go
@@ -1,12 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-func main(){
+func main() {
 	r := gin.Default()
+	type authHeader struct {
+		AuthorizationHeader string `header:"Authorization"`
+	}
+
 	r.GET("/customer", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "customer",
+		h := authHeader{}
+		if err := c.ShouldBindHeader(&h); err != nil {
+			c.JSON(401, gin.H{
+				"message": "Unauthorized",
+			})
+			return
+		}
+		if h.AuthorizationHeader == "123" {
+			c.JSON(200, gin.H{
+				"message": "customer",
+			})
+			return
+		}
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
+		})
+	})
+	r.GET("/product", func(c *gin.Context) {
+		h := authHeader{}
+		if err := c.ShouldBindHeader(&h); err != nil {
+			c.JSON(401, gin.H{
+				"message": "Unauthorized",
+			})
+			return
+		}
+		if h.AuthorizationHeader == "123" {
+			c.JSON(200, gin.H{
+				"message": "product",
+			})
+			return
+		}
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
 		})
 	})
 	err := r.Run("localhost:8888")
